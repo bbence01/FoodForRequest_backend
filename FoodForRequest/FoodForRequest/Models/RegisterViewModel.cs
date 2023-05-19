4187fc67664fc8ea59a31df9bce5377ae1d5e5b2
Bognár Bence <bbence210@gmail.com>
2023-05-19 20:03:47 +0200

    fixed error

--- FoodForRequest/FoodForRequest/Models/RegisterViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FoodForRequest.Models
-{
-    public class RegisterViewModel
-    {
-        public string Email { get; set; }
-        public string FoodUserName { get; set; }
-        public string Password { get; set; }
-    }
-}
